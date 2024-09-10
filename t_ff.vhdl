c53c36938d5c3b62789eca411ff7394c9f0ecb74
vinivid <frato.vini@gmail.com>
2024-09-10 15:09:40 -0300

    afad

--- t_ff.vhdl
+++ t_ff.vhdl
@@ -1,8 +1,28 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity t_ff is
     port (
-        
+        T : in std_logic;
+        Clk : in std_logic;
+        Q : out std_logic;
+        Qcon : out std_logic
     );
 end entity t_ff;
+
+architecture Behaviour of t_ff is
+    
+begin
+    if (rising_edge(Clk)) then
+        if (T = '0') then 
+            T <= T;
+        else
+            T <= not T;
+        end if;
+    end if;
+
+    Q <= T;
+    Qcon <= not T;
+
+end architecture Behaviour;
